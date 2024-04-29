0138abe04b779c416b36203bcdd0724e31f26d78
Avinash <90096511+Av1-Lv5@users.noreply.github.com>
2024-04-29 09:28:01 +0530

    Update ShareBtn.tsx

--- components/ShareBtn.tsx
+++ components/ShareBtn.tsx
@@ -5,6 +5,7 @@ function ShareBtn() {
   return (
     <Button className="bg-white hover:outline hover:outline-black hover:bg-white p-2 w-8 h-8 rounded-full cursor-pointer">
       <Image
+        unoptimized
         className="w-4 h-4"
         width={13}
         height={12}
