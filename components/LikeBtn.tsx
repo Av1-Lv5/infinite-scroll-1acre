4e44db147fb3eb8a6b13994825e10d6b5d71d013
Avinash <90096511+Av1-Lv5@users.noreply.github.com>
2024-04-29 09:28:17 +0530

    Update LikeBtn.tsx

--- components/LikeBtn.tsx
+++ components/LikeBtn.tsx
@@ -5,6 +5,7 @@ function LikeBtn() {
   return (
     <Button className="bg-white hover:outline hover:outline-black hover:bg-white w-8 h-8 p-2 rounded-full cursor-pointer">
       <Image
+        unoptimized
         className="w-4 h-4"
         width={26}
         height={26}
